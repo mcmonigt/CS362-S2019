6cf21b94fb3ba0c698d07b2cafdad5de65980be4
mcmonigt <mcmonigt@oregonstate.edu>
2019-05-19 17:35:12 -0700

    first commit

---
+++ projects/mcmonigt/dominion/randomtestadventurer.c
@@ -0,0 +1,26 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//
+
+// Testing adventurer card
+int main(){
+
+  // initializes gameState
+  struct gameState pre;
+  memcpy(&pre, post)
+
+
+ // initializes other variables necessary for testing
+ int currentPlayer = 0;
+ int temphand[MAX_HAND];
+ int drawntreasure = 0;
+
+
+
+}
